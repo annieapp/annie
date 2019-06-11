9e0aa67c8ef9b52deaa82e2f02fa8993d73ec0b0
Reece Dunham <me@rdil.rocks>
2019-06-11 14:47:54 -0700

    allow opting out

--- onpage/script.js
+++ onpage/script.js
@@ -1,1 +1,3 @@
-new XMLHttpRequest().open("GET","https://your-analytics-server-ip/ping", true).send();
+if(!document.cookie.split("; ")[0]=="annie_optout=yes"){
+  //new XMLHttpRequest().open("GET","https://your-analytics-server-ip/ping", true).send();
+}
