65a404a84e5b7bfb6150d271671a5b323a8085af
Reece Dunham <me@rdil.rocks>
2019-06-11 14:28:26 -0400

    use /status as /

--- server/server.py
+++ server/server.py
@@ -4,7 +4,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/status", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def status():
     return "{'status':'analytics server online'}"
 
