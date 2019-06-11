dcd73f430e88c54fc66f41fc931aa427e5aeb711
Reece Dunham <me@rdil.rocks>
2019-06-11 15:46:15 -0400

    Restrict posting

--- server/server.py
+++ server/server.py
@@ -4,7 +4,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
 def base():
     return "{'status':'analytics server online'}"
 
