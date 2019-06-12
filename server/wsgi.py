3218b4a403820d1457fe59fe3d6b2c7d61e03581
Reece Dunham <me@rdil.rocks>
2019-06-12 13:02:36 -0400

    make wsgi file more clear

--- server/wsgi.py
+++ server/wsgi.py
@@ -1,10 +1,14 @@
-# Uncomment this if you are running your own Annie instance, otherwise
-# just ignore this file
-#import sys
+# This is a template for a WSGI configuration file
+# If you know what that is, great!
+# If you don't, just ignore it (unless you need to run your own Annie instance)
+# Documentation: https://modwsgi.readthedocs.io/en/develop/index.html
 
-#project_home = u'/path/to/annie/server'
-#if project_home not in sys.path:
-#    sys.path = [project_home] + sys.path
+# -----------------------------------------------------------------------
 
-# import flask app but need to call it "application" for WSGI to work
-#from server import app as application  # Do NOT remove, it is used
+import sys
+
+project_home = u'/path/to/annie/server'
+if project_home not in sys.path:
+    sys.path = [project_home] + sys.path
+
+from server import app as application  # Do NOT remove, it is used by WSGI
