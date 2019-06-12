cd93e12f560b5d3cab3bcf1bc4801254c6032b22
Reece Dunham <me@rdil.rocks>
2019-06-12 10:43:54 -0400

    WSGI template

---
+++ server/wsgi.py
@@ -0,0 +1,10 @@
+# Uncomment this if you are running your own Annie instance, otherwise
+# just ignore this file
+#import sys
+
+#project_home = u'/path/to/annie'
+#if project_home not in sys.path:
+#    sys.path = [project_home] + sys.path
+
+# import flask app but need to call it "application" for WSGI to work
+#from server import app as application  # Do NOT remove, it is used
