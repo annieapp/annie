037bfd1f603149a3f4954eba182a81255efe9abb
Reece Dunham <me@rdil.rocks>
2019-06-12 15:34:35 -0400

    Work on config

---
+++ server/config.py
@@ -0,0 +1,9 @@
+from lcbools import true, false
+
+# feel free to adjust these values to whatever you want
+
+# lots of logging output
+verbose = true
+
+# require manual token making
+manual_keygen = false
