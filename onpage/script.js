8cf006ccd7aa27b5c012a32ce6874de223a6c7e3
Reece Dunham <me@rdil.rocks>
2019-06-11 18:57:48 -0700

    uncomment

--- onpage/script.js
+++ onpage/script.js
@@ -1,3 +1,1 @@
-if(!document.cookie.split("; ")[0]=="annie_optout=yes"){
-  //new XMLHttpRequest().open("GET","https://your-analytics-server-ip/ping", true).send();
-}
+if(!document.cookie.split("; ")[0] == "annie_optout=yes") new XMLHttpRequest().open("GET", "https://your-analytics-server-ip/ping", true).send();
