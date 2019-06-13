dc36ca75a723a8b2485069e77275fe9c49854999
Reece Dunham <me@rdil.rocks>
2019-06-13 08:18:02 -0400

    Work on custom 404 page

---
+++ server/statichtml.py
@@ -0,0 +1,21 @@
+fourohfour = """
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>404</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://annieapp.co/css/stylesheet.css">
+  </head>
+  <body>
+    <div class="maine-deo">
+      <div class="col-md-8 col-xs-12">
+        <div class="text-dec">
+        </div>
+      </div>
+    </div>
+    <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
+  </body>
+</html>
+"""
