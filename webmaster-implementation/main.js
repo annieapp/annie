dcf83201f573c7e41dfc4d61353ba212c68982e6
Reece Dunham <me@rdil.rocks>
2019-06-13 14:32:13 -0400

    remove js until done

--- webmaster-implementation/main.js
+++
@@ -1,5 +0,0 @@
-// Warning: the if-statement determines if the user has opted out of Annie data collection or not
-// Removing it is in violation of our ToS
-if(!document.cookie.split("; ")[0] == "annie_optout=yes") {
-  new XMLHttpRequest().open("GET", "https://api.annieapp.co/ping?dataset=YOUR_PUBLIC_KEY", true).send();
-}
