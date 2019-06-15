3fb6612b66bd0bd2500363c00124616829ff4019
Reece Dunham <me@rdil.rocks>
2019-06-15 14:31:56 -0700

    0.2

--- setup.py
+++ setup.py
@@ -21,9 +21,50 @@ FROM THE ANNIE TEAM.
 
 import setuptools
 
+CLASSIFIERS = [
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3 :: Only",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: Implementation :: PyPy",
+    "Operating System :: Microsoft",
+    "Operating System :: Microsoft :: Windows :: Windows 10",
+    "Operating System :: Microsoft :: Windows :: Windows 8",
+    "Operating System :: Microsoft :: Windows :: Windows 8.1",
+    "Operating System :: Microsoft :: Windows :: Windows 7",
+    "Operating System :: MacOS",
+    "Operating System :: MacOS :: MacOS X",
+    "Operating System :: POSIX :: Linux",
+    "Operating System :: Unix",
+    "Operating System :: Other OS",
+    "Intended Audience :: Developers",
+    "Intended Audience :: System Administrators",
+    "Intended Audience :: Information Technology",
+    "Intended Audience :: Science/Research",
+    "Topic :: Software Development :: Libraries",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Topic :: Utilities",
+    "Topic :: System",
+    "Topic :: Terminals",
+    "Topic :: Text Processing",
+    "Topic :: Internet",
+    "Topic :: Internet :: WWW/HTTP :: WSGI",
+    "Topic :: Internet :: WWW/HTTP :: WSGI :: Server",
+    "Topic :: System :: Monitoring",
+    "Topic :: System :: Software Distribution",
+    "Development Status :: 4 - Beta",
+    "Framework :: IDLE",
+    "Framework :: Flask",
+    "Natural Language :: English",
+    "Environment :: Web Environment"
+]
+
 setuptools.setup(
     name='annie-server',
-    version='0.1',
+    version='0.2',
     author="Annie Team",
     description="Annie Server",
     license="See https://github.com/annieapp/annie/blob/master/LICENSE",
@@ -35,5 +76,8 @@ setuptools.setup(
     install_requires=[
         "Flask==1.0.3",
         "lcbools>=1.0.2"
-    ]
+    ],
+    classifiers=CLASSIFIERS,
+    long_description="See https://annieapp.co",
+    long_description_content_type="text/markdown"
 )
