8d2ef629308a7456e862276fd94d1652dc535c6a
Reece Dunham <me@rdil.rocks>
2019-06-16 15:02:29 -0700

    bump version

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ CLASSIFIERS = [
 
 setuptools.setup(
     name='annie-server',
-    version='1.0.1',
+    version='1.1.0',
     author="Annie Team",
     description="Annie Server",
     license="See https://github.com/annieapp/annie/blob/master/LICENSE",
