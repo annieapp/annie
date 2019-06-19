f8d8b5c1f0202fa85bcface708d4ae96eedc194b
Reece Dunham <me@rdil.rocks>
2019-06-19 09:56:02 -0400

    new unit tests

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
 import logging
+import json
 from lcbools import true, false
 
 try:
@@ -10,7 +11,15 @@ except ImportError:
 class Tests(unittest.TestCase):
     def setUp(self):
         self.app = server.application
+        self.app.config['TESTING'] = True
+        self.client = self.app.test_client()
 
+    def test_env(self):
+        self.assertTrue(self.app.config['TESTING'])
+
+    def test_basic_app_values(self):
+        self.assertIsNotNone(self.client)
+        
     def test_key_generation(self):
         self.assertEqual(len(server.genkey()), 15)
 
@@ -21,6 +30,45 @@ class Tests(unittest.TestCase):
     def test_logger(self):
         self.assertIsInstance(self.app.logger, logging.Logger)
         self.assertIsNotNone(self.app.logger)
+
+    @unittest.skipIf(os.getenv("UNITTESTS_FAST_RUN") != None, "Fast run on")
+    def test_status_endpoint(self):
+        self.assertEqual(
+            json.load(self.client.get("/")),
+            json.dumps({
+                "status": "analytics server online"
+            })
+        )
+
+    @unittest.skipIf(os.getenv("UNITTESTS_FAST_RUN") != None, "Fast run on")
+    def test_key_creation(self):
+        data = json.load(self.client.get("/keys/new"))
+        self.tempauth = [
+            data['result']['auth']['key']
+            data['result']['auth']['private-key']
+        ]
+        for i, z in enumerate(self.tempauth):
+            self.assertIsNotNone(self.tempauth[i])
+
+    @unittest.skipIf(os.getenv("UNITTESTS_FAST_RUN") != None, "Fast run on")
+    def test_key_connection(self):
+        for i in range(18):
+            jdata = json.load(self.client.get("/connect", params={"key": self.tempauth[0]})),
+            self.assertIn(
+                jdata['result']
+                {"fail": false}
+            )
+
+    @unittest.skipIf(os.getenv("UNITTESTS_FAST_RUN") != None, "Fast run on")
+    def test_key_deletion(self):
+        self.assertEqual(
+            json.load(self.client.get("/keys/delete", params={"key": self.tempauth[0], "private": self.tempauth[1]})),
+            json.dumps({
+                "result": {
+                    "fail": false
+                }
+            })
+        )
 
 
 if __name__ == '__main__':
