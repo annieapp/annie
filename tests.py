6071ef25c9e9bf46b27334e86927a702405381c7
Reece Dunham <me@rdil.rocks>
2019-06-19 10:10:37 -0400

    aha

--- tests.py
+++ tests.py
@@ -35,7 +35,7 @@ class Tests(unittest.TestCase):
     @unittest.skipIf(os.getenv("UNITTESTS_FAST_RUN") != None, "Fast run on")
     def test_status_endpoint(self):
         self.assertEqual(
-            json.load(self.client.get("/").data),
+            json.load(self.client.get("/").data.decode('utf-8')),
             json.dumps({
                 "status": "analytics server online"
             })
