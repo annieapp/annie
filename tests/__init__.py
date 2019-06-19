2e83d67e1a4558ce53d0683b3cb13e7b0881d3a7
Reece Dunham <me@rdil.rocks>
2019-06-18 19:22:02 -0700

    basic unit tests

---
+++ tests/__init__.py
@@ -0,0 +1,48 @@
+import unittest
+import logging
+from flask import Response
+try:
+    import server
+except ImportError, ModuleNotFoundError:
+    raise OSError("Failed to import the server module. Have you installed it?")
+
+class Tests(unittest.TestCase):
+    def setUp(self):
+        self.app = server.application
+
+    def key_generation(self):
+        self.assertEqual(len(self.app.genkey()), 15)
+
+    def common_errors(self):
+        self.assertEqual(
+            Response(
+                json.dumps({
+                    "result": {
+                        "fail": true,
+                        "message": "Invalid or missing public key"
+                    }
+                }),
+                mimetype='application/json'
+            ),
+            self.app.public_key_error()
+        )
+        self.assertEqual(
+            Response(
+                json.dumps({
+                    "result": {
+                        "fail": true,
+                        "message": "Invalid or missing private key"
+                    }
+                }),
+                mimetype='application/json'
+            ),
+            self.app.private_key_error()
+        )
+
+    def logger(self):
+        self.assertIsInstance(self.app.logger, logging.Logger)
+        self.assertNotNone(self.app.logger)
+
+
+if __name__ == '__main__':
+    unittest.main()
